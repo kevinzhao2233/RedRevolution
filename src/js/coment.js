578f9930eaf97c447053dba44ece25c63816a80f
kevin <2939575326@qq.com>
2019-10-13 20:11:58 +0800

    完善弹幕功能

--- src/js/coment.js
+++ src/js/coment.js
@@ -6,23 +6,14 @@ new Valine({
   placeholder: '在这里写下想对祖国或者先烈们说的话吧，最多50个字符哦' // 留言框占位提示文字
 });
 
+// 为用户体验，限制评论的长度为50字；
 const editor = document.querySelector('#veditor');
-if(editor) {
+if (editor) {
   editor.maxLength = 50;
 }
-console.log(editor.maxLength)
+console.log(editor.maxLength);
 
-/**
- *思路
- * 在页面load后，使用延时加载评论（这个日后可以改一改，看看能不能监听请求）
- * 将评论连同 p 标签保存到 comment[] 
- * 弹幕尝试使用 requestAnimationFrame，增加页面流畅度
- * 先让一个子弹左边对齐父容器右边缘，然后计算父容器宽度（1000px）+ 子弹宽度，
- * 这样就能确定一个子弹经过的时间（速度固定）
- * 为子弹添加的动画尽量是 transform，不要用left等等
- */
-
-const getElementProp = () => {
+function getElementProp() {
   const barrage = document.querySelector('#barrage');   // 弹幕的大盒子
   const track = document.querySelectorAll('.track');     // 弹幕弹道
   const bullet = document.querySelectorAll('.track p');   // 子弹
@@ -44,21 +35,30 @@ const getComment = () => {
   console.log(comment);
 }
 
-const rate = 1;     // 速度
 
-const barrageAnimation = () => {
+const barrageAnimation = (newComment) => {
   const elementProp = getElementProp();
-  const tempComment = [...comment];
+  const tempComment = [...comment];   // 用来循环的评论数组，copy
+  let index = 0;
+  if (!!newComment) {
+    tempComment.splice(index + 1, 0, `<p>${newComment}</p>`);
+  }
   const bulletMarginR = getComputedStyle(elementProp.bullet[0], null).marginRight; // 子弹 margin-right
   setInterval(() => {
     for (let track of elementProp.track) {      // 遍历轨道
       if (track.lastElementChild) {
+        const comm = tempComment[index];
+        index++;
+        if (index >= tempComment.length) {
+          index = 0;
+        }
+
         const lastElem = {
           clientLeft: track.lastElementChild.getBoundingClientRect().left,
           width: track.lastElementChild.clientWidth
         }
         if ((lastElem.clientLeft + lastElem.width + parseInt(bulletMarginR)) < (barrage.clientWidth + barrage.getBoundingClientRect().left)) {
-          track.appendChild(createDocumentFragment(tempComment[1]));
+          track.appendChild(createDocumentFragment(comm));
         }
       } else {
         // TODO: 如果所有轨道都没有子弹
@@ -70,15 +70,20 @@ const barrageAnimation = () => {
         }
         if ((firstEle.clientLeft + firstEle.width + parseInt(bulletMarginR)) < (barrage.getBoundingClientRect().left)) {
           track.removeChild(track.firstElementChild);
-          console.log('可以删掉的');
         }
       }
     }
-  }, 500);
+  }, 800);
 }
 
+const btnSubmit = document.querySelector('.vsubmit');
+btnSubmit.addEventListener('click', function () {
+  let submitValue = document.querySelector('#veditor').value;
+  console.log(submitValue, 'aaaaaa');
+  barrageAnimation(submitValue);
+})
 
-setTimeout(() => {    //TODO: 延迟，让JS能够加载到评论，这个以后可以改一下
+setTimeout(() => {
   getComment();
   barrageAnimation();
-}, 1000);
+}, 1200);
